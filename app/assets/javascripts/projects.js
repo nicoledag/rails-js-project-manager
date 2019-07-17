3301beac56b9d099405dc478e64970ef1f59bbe7
github username <github email address>
2019-07-17 13:47:56 -0400

    Update project click Handler to append data to site

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -22,12 +22,32 @@
           .then(projects => {
             // console.log(projects);
             $('#app-container').html('')
+            projects.forEach(project => {
+              let newProject = new Project(project)
+              // console.log(newProject);
+              let postHtml = newProject.formatIndex()
+              // console.log(postHtml);
+              $('#app-container').append(postHtml)
+            })
           })
-
-
-
-
       })
     }
+
+  function Project(project) {
+    this.id = project.id
+    this.name = project.name
+    this.description = project.description
+    this.target_completion_date = project.target_completion_date
+    this.completion_date = project.completion_date
+  }
+
+  Project.prototype.formatIndex = function () {
+    let postHtml = `
+      <h1>${this.name}</h1>
+    `
+    return postHtml
+  }
+
+
 
 }(window, jQuery));
