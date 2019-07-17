20e7e642e3bdd524c36e4f45cdd319178f617e26
github username <github email address>
2019-07-17 13:31:12 -0400

    Add fetch to projectClickHandler

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -9,8 +9,22 @@
 
     const projectClickHandler = () => {
       $('.all_projects').on('click', e => {
-        e.preventDefault()
-        console.log('hello');
+        e.preventDefault()  //prevent default from running.
+        // hijacks nav link for projects.  On method takes in two arguements the click and event
+        // and a callback function you want to run when the event happens.
+        // console.log(hello);
+        fetch(`/projects.json`)
+        // fetch makes request to backend.  When you call fetch you get a promise.
+        // if resolved it takes in a response.  Call json on the resonse and it will parse the data on the response.
+        // then passes to next then call.
+          .then(res => res.json())
+          .then(projects => {
+            // console.log(projects);
+          })
+
+
+
+
       })
     }
 
