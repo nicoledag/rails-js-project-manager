adbea1c92cce84317f4733523de9290ad72bdb39
github username <github email address>
2019-07-18 15:02:27 -0400

    Update table to show projects using prototype method

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,19 +1,29 @@
-(function(global, $) {
 
 
   $(() => {
       console.log( "ready!" );
       projectClickHandler()
+      dateGreeting()
   })
+
+
+  const dateGreeting = () => {
+    let d = new Date();
+    let date = $(".date").innerHTML = "<h3>Today's date is " + d + "</h3>"
+    $('.date').append(date)
+  }
+
 
 
     const projectClickHandler = () => {
       $('.all_projects').on('click', e => {
         e.preventDefault()  //prevent default from running.
+        // console.log('hello');
+
         // hijacks nav link for projects.  On method takes in two arguements the click and event
         // and a callback function you want to run when the event happens.
-        // console.log(hello);
-        history.pushState(null, null, "projects") //updates url path to /projects.
+
+        // history.pushState(null, null, "projects") //updates url path to /projects.
         fetch(`/projects.json`)
         // fetch makes request to backend.  When you call fetch you get a promise.
         // if resolved it takes in a response.  Call json on the resonse and it will parse the data on the response.
@@ -21,58 +31,93 @@
           .then(res => res.json())
           .then(projects => {
             // console.log(projects);
-            $('#app-container').html('')
+            // $('#app-container').html('')
             projects.forEach(project => {
               let newProject = new Project(project)
-              // console.log(newProject);
+              console.log(newProject);
+
               let postHtml = newProject.formatIndex()
-              // console.log(postHtml);
-              $('#app-container').append(postHtml)
+
+              console.log(postHtml);
+            $('#table').append(postHtml)
+
             })
-          })
+
+         })
+
       })
     }
 
+
   function Project(project) {
-    this.id = project.id
     this.name = project.name
     this.description = project.description
     this.target_completion_date = project.target_completion_date
     this.completion_date = project.completion_date
+    this.company_name = project.client.company_name
+    this.content = project.comments.content
   }
 
   Project.prototype.formatIndex = function () {
-    let postHtml = `
-    <div style="overflow-x:auto;">
-      <div class="scrollit">
-        <table id="table">
-          <tr>
-            <th>Project Name</th>
-            <th>Description</th>
-            <th>Target Completion Date</th>
-            <th>Completion Date</th>
-          </tr>
 
-          <tr>
-           <td>${this.name}</td>
-           <td>${this.description}</td>
-           <td>${this.target_completion_date} </td>
-           <td>${this.completion_date} </td>
-         </tr>
-        </table>
-       </div>
-    </div>
+    let postHtml = `
+      <tr>
+      <td>${this.name} </td>
+      <td>${this.description} </td>
+      <td>${this.company_name} </td>
+      <td>${this.target_completion_date} </td>
+      <td>${this.completion_date} </td>
+      <td>${this.content} </td>
+
+      </tr>
     `
     return postHtml
   }
+  //
 
-  // Project.prototype.completedProjects = function () {
+
+
+  function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+
+  function generateTable(table, values) {
+  for (let element of values) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element);
+      cell.appendChild(text);
+    }
+  }
+}
+
+
+  // let table = document.querySelector("#table");
+  // let data = Object.keys(postHtml);
+  // let values = Object.values(postHtml);
+  // console.log(values);
+  //
+  // generateTableHead(table, data);
+  // generateTable(table, values);
+  //
+
+
+
+
+
+
+  // Project.prototype.sortByCompletedDate = function () {
   //   const completed = Project.filter(function(project) {
   //         project.completion_date !== null
   //         console.log(completed);
   //   });
   //
   // }
-
-
-}(window, jQuery));
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -2,4 +2,5 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :target_completion_date, :completion_date
   has_many :comments
   belongs_to :user
+  belongs_to :client
 end
