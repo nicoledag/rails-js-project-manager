8311c5438a93cd4245f0c49bbc78c691763978d7
github username <github email address>
2019-07-19 10:55:03 -0400

    Work on greeting function

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,11 +1,27 @@
 
-
   $(() => {
       console.log( "ready!" );
       // projectClickHandler()
       listenForClick()
       dateGreeting()
+      // nameGreeting()
   })
+
+  // const nameGreeting = () => {
+  //      fetch(`/projects.json`)
+  //     .then(res => res.json())
+  //     .then(projects => {
+  //            // console.log(projects);
+  //            projects.forEach(project => {
+  //            let newProject = new Project(project)
+  //            // console.log(newProject);
+  //            let postName = newProject.Greeting()
+  //            // console.log(postName);
+  //            $('#greeting').append(postName)
+  //            })
+  //        })
+  //    }
+  //
 
 
   const dateGreeting = () => {
@@ -33,40 +49,6 @@
    }
 
 
-    // const projectClickHandler = () => {
-    //   $('.all_projects').on('click', e => {
-    //     e.preventDefault()  //prevent default from running.
-    //     // console.log('hello');
-    //
-    //     // hijacks nav link for projects.  On method takes in two arguements the click and event
-    //     // and a callback function you want to run when the event happens.
-    //
-    //     history.pushState(null, null, "projects") //updates url path to /projects.
-    //     fetch(`/projects.json`)
-    //     // fetch makes request to backend.  When you call fetch you get a promise.
-    //     // if resolved it takes in a response.  Call json on the resonse and it will parse the data on the response.
-    //     // then passes to next then call.
-    //       .then(res => res.json())
-    //       .then(projects => {
-    //         // console.log(projects);
-    //         // $('#app-container').html('')
-    //         projects.forEach(project => {
-    //           let newProject = new Project(project)
-    //           console.log(newProject);
-    //
-    //           let postHtml = newProject.formatIndex()
-    //
-    //           console.log(postHtml);
-    //         $('#table').append(postHtml)
-    //
-    //         })
-    //
-    //      })
-    //
-    //   })
-    // }
-
-
   function Project(project) {
     this.name = project.name
     this.description = project.description
@@ -74,11 +56,11 @@
     this.completion_date = project.completion_date
     this.company_name = project.client.company_name
     this.comments = project.comments
+    this.username = project.user.username
   }
 
   Project.prototype.formatIndex = function () {
-
-    let postHtml = `
+    let postName = `
       <tr>
       <td>${this.name} </td>
       <td>${this.description} </td>
@@ -86,12 +68,17 @@
       <td>${this.target_completion_date} </td>
       <td>${this.completion_date} </td>
       <td>${this.comments} </td>
-
       </tr>
     `
     return postHtml
   }
 
+  // Project.prototype.Greeting = function () {
+  //   let postName = `
+  //     <h2> Welcome ${this.username} </h2>
+  //   `
+  //   return postName
+  // }
 
 
 
