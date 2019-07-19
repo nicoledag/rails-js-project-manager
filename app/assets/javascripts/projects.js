92b2d15e02d58cf4922d138026f5a469fd2d2b06
github username <github email address>
2019-07-19 11:45:05 -0400

    Put hideButton in own function

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,28 +1,9 @@
 
   $(() => {
       console.log( "ready!" );
-      // projectClickHandler()
       listenForClick()
       dateGreeting()
-      // nameGreeting()
   })
-
-  // const nameGreeting = () => {
-  //      fetch(`/projects.json`)
-  //     .then(res => res.json())
-  //     .then(projects => {
-  //            // console.log(projects);
-  //            projects.forEach(project => {
-  //            let newProject = new Project(project)
-  //            // console.log(newProject);
-  //            let postName = newProject.Greeting()
-  //            // console.log(postName);
-  //            $('#greeting').append(postName)
-  //            })
-  //        })
-  //    }
-  //
-
 
   const dateGreeting = () => {
     let d = new Date();
@@ -33,8 +14,18 @@
   function listenForClick() {
     $('button#post-data-all-projects').on('click', e => {
       event.preventDefault()
+      hideButton()
       getAllProjects()
     })
+  }
+
+  function hideButton() {
+    var x = document.getElementById("table-js");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
 
   function getAllProjects() {
@@ -45,22 +36,24 @@
            projects.map(project => {
              let newProject = new Project(project)
               // console.log(newProject);
+            // let formatByDate = newProject.formatDate()
              let postHtml = newProject.formatIndex()
-             console.log(postHtml);
              $('#table-js').append(postHtml)
            })
        })
    }
 
-
-  function Project(project) {
-    this.name = project.name
-    this.description = project.description
-    this.target_completion_date = project.target_completion_date
-    this.completion_date = project.completion_date
-    this.company_name = project.client.company_name
-    this.comments = project.comments
-    this.username = project.user.username
+   class Project {
+    constructor(project) {
+       this.name = project.name
+       this.description = project.description
+       this.target_completion_date = project.target_completion_date
+       this.completion_date = project.completion_date
+       this.company_name = project.client.company_name
+       this.comments = project.comments
+       this.username = project.user.username
+       this.created_at = project.created_at
+   }
   }
 
   Project.prototype.formatIndex = function () {
@@ -75,12 +68,11 @@
     `
     return postHtml
   }
-
-  // Project.prototype.Greeting = function () {
-  //   let postName = `
-  //     <h2> Welcome ${this.username} </h2>
-  //   `
-  //   return postName
+  //
+  // Project.prototype.formatDate = function () {
+  //  Project.sort(function(a,b){
+  //  return a.created_at > b.created_at;
+  //   })
   // }
 
 
--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :target_completion_date, :completion_date
+  attributes :created_at, :id, :name, :description, :target_completion_date, :completion_date
   has_many :comments
   belongs_to :user
   belongs_to :client
