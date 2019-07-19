844d1793a434e1d0fd99bdb9300c2054ebd1745e
github username <github email address>
2019-07-19 10:23:54 -0400

    Add button click event and function to get all projects

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,7 +2,8 @@
 
   $(() => {
       console.log( "ready!" );
-      projectClickHandler()
+      // projectClickHandler()
+      listenForClick()
       dateGreeting()
   })
 
@@ -13,40 +14,57 @@
     $('.date').append(date)
   }
 
+  function listenForClick() {
+    $('button#post-data-all-projects').on('click', e => {
+      event.preventDefault()
+      getAllProjects()
+    })
+  }
+
+  function getAllProjects() {
+    fetch(`/projects.json`)
+    .then(res => res.json())
+    .then(projects => {
+           console.log(projects);
+           projects.forEach(project => {
+
+           })
+       })
+   }
 
 
-    const projectClickHandler = () => {
-      $('.all_projects').on('click', e => {
-        e.preventDefault()  //prevent default from running.
-        // console.log('hello');
-
-        // hijacks nav link for projects.  On method takes in two arguements the click and event
-        // and a callback function you want to run when the event happens.
-
-        history.pushState(null, null, "projects") //updates url path to /projects.
-        fetch(`/projects.json`)
-        // fetch makes request to backend.  When you call fetch you get a promise.
-        // if resolved it takes in a response.  Call json on the resonse and it will parse the data on the response.
-        // then passes to next then call.
-          .then(res => res.json())
-          .then(projects => {
-            // console.log(projects);
-            // $('#app-container').html('')
-            projects.forEach(project => {
-              let newProject = new Project(project)
-              console.log(newProject);
-
-              let postHtml = newProject.formatIndex()
-
-              console.log(postHtml);
-            $('#table').append(postHtml)
-
-            })
-
-         })
-
-      })
-    }
+    // const projectClickHandler = () => {
+    //   $('.all_projects').on('click', e => {
+    //     e.preventDefault()  //prevent default from running.
+    //     // console.log('hello');
+    //
+    //     // hijacks nav link for projects.  On method takes in two arguements the click and event
+    //     // and a callback function you want to run when the event happens.
+    //
+    //     history.pushState(null, null, "projects") //updates url path to /projects.
+    //     fetch(`/projects.json`)
+    //     // fetch makes request to backend.  When you call fetch you get a promise.
+    //     // if resolved it takes in a response.  Call json on the resonse and it will parse the data on the response.
+    //     // then passes to next then call.
+    //       .then(res => res.json())
+    //       .then(projects => {
+    //         // console.log(projects);
+    //         // $('#app-container').html('')
+    //         projects.forEach(project => {
+    //           let newProject = new Project(project)
+    //           console.log(newProject);
+    //
+    //           let postHtml = newProject.formatIndex()
+    //
+    //           console.log(postHtml);
+    //         $('#table').append(postHtml)
+    //
+    //         })
+    //
+    //      })
+    //
+    //   })
+    // }
 
 
   function Project(project) {
@@ -73,43 +91,6 @@
     `
     return postHtml
   }
-  //
-
-
-
-  function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
-    }
-  }
-
-  function generateTable(table, values) {
-  for (let element of values) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element);
-      cell.appendChild(text);
-    }
-  }
-}
-
-
-  // let table = document.querySelector("#table");
-  // let data = Object.keys(postHtml);
-  // let values = Object.values(postHtml);
-  // console.log(values);
-  //
-  // generateTableHead(table, data);
-  // generateTable(table, values);
-  //
-
-
 
 
 
